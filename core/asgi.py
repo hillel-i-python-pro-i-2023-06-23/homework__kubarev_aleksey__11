98bc4277de6e5dfc04f7838ebae49fe058ad158e
disna <alex.kub.05@gmail.com>
2023-08-03 16:45:39 +0300

    start project

--- core/asgi.py
+++ core/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for homework__kubarev_aleksey__11 project.
+ASGI config for shared__django__example_2023_06_23 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homework__kubarev_aleksey__11.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = get_asgi_application()
--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for homework__kubarev_aleksey__11 project.
+URL configuration for shared__django__example_2023_06_23 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+# from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path("", include("apps.base.urls")),
+    path("first-example/", include("apps.first_example.urls")),
+    path("animals/", include("apps.animals.urls")),
 ]
